112dfad33f46dac4d1489d6fee761f76dd971822
Scott Lott <me@scottlott.com>
2021-11-07 17:35:37 -0800

    initial release

---
+++ run.js
@@ -0,0 +1,187 @@
+const path = require("path");
+const fs = require("fs");
+const child = require("child_process");
+
+const package = process.argv[2];
+
+const package_path = package.split("/").filter(f => f && f.length);
+
+const full_path = (partial_path) => path.join(__dirname, "libs", partial_path.join(path.sep));
+
+const package_root = path.join(__dirname, "libs", package);
+
+child.execSync(`npm install ${package}`);
+try {
+    child.execSync(`npm install @types/${package}`);
+} catch (e) {}
+
+const copy_types = (scan_dir, subdirs, extension, recursive) => {
+    const files = fs.readdirSync(path.join(scan_dir, ...subdirs));
+    try {
+        fs.mkdirSync(path.join(package_root, ...subdirs));
+    } catch (e) { }
+
+    let copy = [];
+    for (key in files) {
+        if (files[key].indexOf(extension) !== -1) {
+            fs.copyFileSync(path.join(scan_dir, ...subdirs, files[key]), path.join(package_root, ...subdirs, files[key]));
+            copy.push(path.join(package_root, ...subdirs, files[key]));
+        } else if (fs.fstatSync(fs.openSync(path.join(scan_dir, ...subdirs, files[key]))).isFile() == false && recursive) {
+            copy.concat(copy_types(scan_dir, [...subdirs, files[key]], extension, true));
+        }
+    }
+    return copy
+}
+
+(async () => {
+    for (let i = 0; i < package_path.length; i++) {
+
+        if (!fs.existsSync(full_path(package_path.slice(0, i + 1)))) {
+            fs.mkdirSync(full_path(package_path.slice(0, i + 1)));
+        }
+    }
+
+    try {
+        fs.unlinkSync(path.join(__dirname, "deps.json"));
+    } catch (e) {
+
+    }
+
+    fs.writeFileSync(path.join(__dirname, "deps.json"), "{}");
+
+    const package_json = JSON.parse(fs.readFileSync(path.join(__dirname, "node_modules", package, "package.json")));
+
+    let entry = path.join(__dirname, `/node_modules/${package}/${package_json.main || "index.js"}`);
+
+    let has_gen_entry = false;
+    if (fs.existsSync(entry) == false) {
+        has_gen_entry = true;
+        entry = path.join(__dirname, `/node_modules/${package}/index.js`);
+        let includes = [];
+        const files = fs.readdirSync(path.join(__dirname, `/node_modules/${package}`));
+        for (key in files) {
+
+            if (fs.fstatSync(fs.openSync(path.join(__dirname, `/node_modules/${package}/${files[key]}`))).isFile() == false) {
+                const subfiles = fs.readdirSync(path.join(__dirname, `/node_modules/${package}/${files[key]}`));
+                let possible_files = subfiles.filter(f => f.indexOf(".js") !== -1 && f.indexOf(".json") === -1).sort((a, b) => a.split("").filter(f => f == ".").length > b.split("").filter(f => f == ".").length ? 1 : -1);
+                if (possible_files.length > 0) {
+                    includes.push([files[key], possible_files[0]]);
+                }
+            }
+        }
+        
+        let new_index = "module.exports = {\n";
+        includes.forEach(([mod, file]) => {
+            new_index += `  ${mod}: require("./${mod}/${file}"),\n`;
+        });
+        new_index += "};";
+
+        fs.writeFileSync(path.join(__dirname, `/node_modules/${package}/index.js`), new_index);
+    }
+
+    if (fs.existsSync(path.join(__dirname, "node_modules", package, "README.md"))) {
+        fs.copyFileSync(path.join(__dirname, "node_modules", package, "README.md"), path.join(full_path(package_path), "README.md"));
+    } else if (fs.existsSync(path.join(__dirname, "node_modules", package, "readme.md"))) {
+        fs.copyFileSync(path.join(__dirname, "node_modules", package, "readme.md"), path.join(full_path(package_path), "README.md"));
+    } else {
+        // no readme
+    }
+
+    // minified build
+    await new Promise((res, rej) => {
+        const prod = child.spawn(`./node_modules/.bin/webpack-cli`, [`--progress`, `--config`, `webpack.config.prod.5.js`, `--env`, `type=prod`, `mod=${package}`], {
+            cwd: process.cwd(),
+            detached: true,
+            stdio: "inherit"
+          });
+
+        prod.on('exit', res);
+    });
+
+    // non minified build
+    await new Promise((res, rej) => {
+        const prod = child.spawn(`./node_modules/.bin/webpack-cli`, [`--progress`, `--config`, `webpack.config.prod.5.js`, `--env`, `type=dev`, `mod=${package}`], {
+            cwd: process.cwd(),
+            detached: true,
+            stdio: "inherit"
+          });
+
+        prod.on('exit', res);
+    });
+
+    const version = child.execSync(`npm view ${package} version`).toString();
+
+    const shasum = child.execSync(`shasum -b -a 512 libs/${package}/index.min.js | awk '{ print $1 }' | xxd -r -p | base64`);
+    const shasumDev = child.execSync(`shasum -b -a 512 libs/${package}/index.js | awk '{ print $1 }' | xxd -r -p | base64`);
+
+    try {
+        fs.unlinkSync(path.join(__dirname, "libs", package, "lib.js"));
+    } catch (e) {
+
+    }
+
+    const sizeProd = fs.readFileSync(path.join(__dirname, "libs", package, "index.min.js")).toString().length;
+    const sizeDev = fs.readFileSync(path.join(__dirname, "libs", package, "index.js")).toString().length;
+
+    // copy css files
+    let css_files = copy_types(path.join(__dirname, "node_modules", package), [], ".css", false);
+
+    fs.writeFileSync(path.join(__dirname, "libs", package, "lib.json"), `
+    {
+        "api": 1,
+        "name": "${package}",
+        "description": "${package_json.description}",
+        "author": ${package_json.author == undefined ? '""' : JSON.stringify(package_json.author)},
+        "version": "${version.slice(0, version.length - 1)}",
+        "repo": ${JSON.stringify(package_json.repository)},
+        "homepage": "${package_json.homepage}",
+        "keywords": ${JSON.stringify(package_json.keywords || [])},
+        "license": "${package_json.license}",
+        "dependencies": ${JSON.stringify(JSON.parse(fs.readFileSync(path.join(__dirname, "deps.json")).toString()), null, 4).replace(/    /img, "        ").replace("}", "    }")},
+        "files": [
+            {"file": "index.min.js", "sri": "sha512-${shasum.slice(0, shasum.length - 1)}", "sizeKB": ${sizeProd/1000}}
+        ],
+        "devFiles": [
+            {"file": "index.js", "sri": "sha512-${shasumDev.slice(0, shasum.length - 1)}", "sizeKB": ${sizeDev/1000}}
+        ]
+    }
+    `.trim());
+
+    try {
+        fs.unlinkSync(path.join(__dirname, "deps.json"));
+    } catch (e) {
+
+    }
+
+    if (has_gen_entry) {
+        fs.unlinkSync(path.join(__dirname, `/node_modules/${package}/index.js`));
+    }
+
+
+
+
+
+    // Checks for TS files and copy them over
+    const types_root = path.join(__dirname, "node_modules", "@types", package);
+
+    // check for ts files
+    if (fs.existsSync(types_root)) {
+        copy_types(types_root, [], ".d.ts", true);
+    } else { // try to find types in node_modules
+        copy_types(path.join(__dirname, "node_modules", package), [], ".d.ts", true);
+    }
+
+    // sometimes node_modules interior directory gets coppied over, remove it if it's there
+    try {
+       fs.rmSync(path.join(__dirname, "libs", package, "node_modules"), {recursive: true });
+    } catch (e) {
+
+    }
+
+    child.execSync(`npm uninstall ${package}`);
+    try {
+        child.execSync(`npm uninstall @types/${package}`);
+    } catch (e) {}
+
+
+})()
---
+++ webpack.config.prod.5.js
@@ -0,0 +1,131 @@
+const path = require("path");
+const fs = require("fs");
+const child = require("child_process");
+const webpack = require('webpack');
+
+// npm view carbon-components version
+// shasum -b -a 512 libs/carbon-components/index.min.js | awk '{ print $1 }' | xxd -r -p | base64
+
+const production = process.argv[process.argv.length - 2].split("=").pop() == "prod" ? true : false;
+
+const module_name = process.argv[process.argv.length - 1].split("=").pop();
+
+if (production) {
+    console.log("Production Build:");
+} else {
+    console.log("Development Build:");
+}
+
+const package_file = JSON.parse(fs.readFileSync(path.join(__dirname, `/node_modules/${module_name}/package.json`)).toString());
+
+const new_package = String(module_name);
+
+module.exports = {
+    mode: production ? "production" : "development",
+    entry: path.join(__dirname, `/node_modules/${module_name}/${package_file.main || "index.js"}`),
+    output: {
+        library: new_package,
+        libraryTarget: 'amd',
+        path: path.resolve(__dirname, 'libs', new_package),
+        filename: `index${production ? ".min": ""}.js`
+    },
+    devtool: false,
+    node: false,
+    optimization: {
+        minimize: production
+    },
+    externals: [
+        function({ context, request }, callback) {
+
+            const paths = ["../", "./"].map(p => request.indexOf(p));
+
+            const depdency = JSON.parse((() => {
+                try {
+                    return fs.readFileSync("deps.json");
+                } catch (e) {
+                    return "{}";
+                }
+            })());
+
+            const is_remote_dependency = () => {
+
+                if (request.indexOf("!!webpack") !== -1) {
+                    return false;
+                }
+                
+                const first_char = Array.from(request)[0];
+                const num_slahes = Array.from(request).filter(f => f == "/").length;
+
+                if (first_char == "/" || first_char == ".") {
+                    return false;
+                } else if (first_char == "@") {
+                    return num_slahes <= 1;
+                } else {
+                    return num_slahes == 0;
+                }
+            };
+
+            if (is_remote_dependency()) {
+            
+
+                if (!depdency[request]) {
+                    try {
+                        const version = package_file.dependencies[request];
+                        depdency[request] = version;
+                        fs.writeFileSync("deps.json", JSON.stringify(depdency, null, 4));
+                    } catch (e) {
+                        try {
+                            child.execSync(`npm install ${request}`);
+                        } catch (e) { }
+                        
+                        const package = JSON.parse(fs.readFileSync(path.join(__dirname, "node_modules", request, "package.json")));
+                        const version = package.version;    
+                        depdency[request] = version;
+                        fs.writeFileSync("deps.json", JSON.stringify(depdency, null, 4));
+
+                        try {
+                            child.execSync(`npm uninstall ${request}`);
+                        } catch (e) { }
+                    }
+
+                }
+
+                return callback(null, 'amd ' + request);
+            }
+
+            // Continue without externalizing the import
+            callback();
+        },
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ["@babel/plugin-proposal-class-properties"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            { 
+                test: /\.tsx?$/, 
+                loader: "ts-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: production ? 'production' : 'development',
+            DEBUG: false
+        }),
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1,
+        }),
+    ]
+};
