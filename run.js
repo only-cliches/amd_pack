57e3f6312b1acc23cdf5ca324765598e88c9c10a
Scott Lott <me@scottlott.com>
2021-11-16 16:30:28 -0800

    added store folder

--- run.js
+++ run.js
@@ -138,6 +138,7 @@ if (process.argv[2] == "pack") {
     scan_files(__cwd, ["pages"]);
     scan_files(__cwd, ["components"]);
     scan_files(__cwd, ["utilities"]);
+    scan_files(__cwd, ["store"]);
 
     const removeEmptyKeys = (obj) => {
         if (Object.keys(obj).length) {
